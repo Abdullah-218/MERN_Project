adeef0fbe2e504510f8f934faa6aac3dda5ab060
AbdlhXDev <abdullahoffl2005@gmail.com>
2025-06-25 20:27:04 +0530

    Adding userRoute function part 2

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,12 +24,66 @@ const createNewUser = asyncHandler(async (requestAnimationFrame,resp)=>{
     if(!username || !password || !Array.isArray(roles) || !roles.length){
         return resp.status(400).json({ message : 'All fields are required !'})
     }
+
+    // Check for duplicate 
+    const duplicate = await User.findOne({username}).lean().exec();
+
+    if(duplicate){
+        return resp.status(409).json({message: 'Duplicate username'})
+    }
+
+    // Hash password
+    const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
+
+    const userObject = { username, "password":hashedPwd, roles}
+
+    // Crete and store new user 
+    const user = await User.cretae(userObject)
+
+    if(user)
+    {
+        resp.status(201).json({ message: `New user ${username} cretaed !`})
+    }else{
+      resp.status(400).json({ message: 'Invalid user data received'})
+    }
+
 })
 
 // @desc Update a user
 // @route Patch /users
 // @access Private
 const updateUser = asyncHandler(async (requestAnimationFrame,resp)=>{
+    const {id, username, roles, active, password } = req.body;
+    if(!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean'){
+        return resp.status(400).json({message : 'All fields are required !'})
+    }
+
+    const user = await User.findById(id).exex()
+
+    if(!user){
+        return resp.status(400).json({message : 'user not. found'})
+    }
+
+    // Duplicate check
+    const duplicate = await User.findOne({username}).lean().exec()
+    // Allow updates to the oriiginal user 
+
+    if(duplicate && duplicate?._id.toString() !== id){
+        return resp.status(409).json({message : 'Duplicate usernme '})
+    }
+
+    user.username = username
+    user.roles = roles
+    user.active = active
+
+    if(password){
+        // Hash password
+        user.password = await bcrypt.hash(password, 10)
+    }
+
+    const updatedUser = await user.save()
+
+    resp.json({ message: `${updatedUser.username} Updated !`})
 
 })
 
@@ -37,7 +91,25 @@ const updateUser = asyncHandler(async (requestAnimationFrame,resp)=>{
 // @route Patch /users
 // @access Private
 const deleteUser = asyncHandler(async (requestAnimationFrame,resp)=>{
+    const { id } = req.body
+    if(!id){
+        return resp.status(400).json({message: ' User Id required !'})
+    }
 
+    const notes = await Note.findOne({user: id}).lean().exec()
+    if(notes?.length){
+        return resp.status(400).json({message: 'User has assigned notes'})
+    }
+
+    const user = await User.findById(id).exec()
+
+    if(!user){
+        return resp.status(400).json({message: 'User not found !'});
+    }
+
+    const result = await user.deleteOne()
+
+    const reply = `Username ${result.username} with ID ${result._id} deleted`
 })
 
 module.exports = {
