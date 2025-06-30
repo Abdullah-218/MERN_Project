d77cd98d43fa32c1edd0bc2f2a0e52dbfca1991e
AbdlhXDev <abdullahoffl2005@gmail.com>
2025-06-30 20:24:06 +0530

    Doing the auth process

---
+++ backEnd/routes/authRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const authController = require('../controllers/authController')
+const loginLimiter = require('../middleware/loginLimiter')
+
+
+router.route('/')
+    .post(loginLimiter)
+
+router.route('/refresh')   
+    .get()
+
+router.route('/logout')
+    .post()
+    
+module.exports = router    
